13599df38848af926a4fd93802f7d8ab24f38f63
Sohamdip-Santra1 <sohamdipsantra2023@gmail.com>
2026-09-09 00:40:44 +0530

    sql queries

--- cephalic_db.sql
+++ cephalic_db.sql
@@ -1,5 +1,22 @@
 
+
+
 select * from "user" as u where u.role='admin'
-select * from sensor_data
+select n.name as Node_name,* from sensor_data as sd , node as n  where (sd.node_id=n.id)
 select * from "user" as u
 select * from (select * from mine) order by id asc
+select * from node
+select distinct mine_id,from_node_id,to_node_id from mine_nodes as m_n join node_links as nl on m_n.mine_id=nl.from_node_id where m_n.mine_id=2 order by nl.to_node_id ASC  
+
+select distinct mine_id,from_node_id,to_node_id from mine_nodes as m_n join node_links as nl on m_n.mine_id=nl.from_node_id order by nl.to_node_id ASC  
+
+
+
+select * from node_links
+select * from user_mines
+
+
+-- for deleting the entier database 
+-- this is mainly for the changing the database model like adding/deleting new column
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
