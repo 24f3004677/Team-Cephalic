d901381de72d745629e981feeefc7d91bb9a4896
Sohamdip-Santra1 <sohamdipsantra2023@gmail.com>
2026-09-12 00:35:17 +0530

    fixed the sqlite db in config.py file

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:Sarbanidutta%40123@localhost:5432/cephalic_db'
-    SQLALCHEMY_DATABASE_URI= 'sqlite:///cephalic_db'
+    SQLALCHEMY_DATABASE_URI= 'sqlite:///cephalic.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     AUTO_CREATE_DB = True
