d5dba17152b36386f2bbfb16db24515cd74df7c9
Prem Karmakar <premkarmakar06@gmail.com>
2026-09-11 15:08:57 +0530

    prem updated

--- file_extractor.sh
+++ file_extractor.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # ====== CONFIG ======
-OUTPUT_DIR="/mnt/c/Users/Sohamdip/Desktop/Data/Projects/SIH_26/output"
+OUTPUT_DIR="/mnt/c/Users/Prem Karmakar/Desktop/DL_backend/output"
 TIMESTAMP="$(date +'%Y-%m-%d_%H-%M-%S')"
 OUTPUT_FILE="$OUTPUT_DIR/code_base_${TIMESTAMP}.txt"
 
