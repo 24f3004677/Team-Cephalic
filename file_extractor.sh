d612bf21a88c449ab08f8989d64ed4499237bd04
Sohamdip-Santra1 <sohamdipsantra2023@gmail.com>
2026-09-13 19:14:22 +0530

    changed the file extractor

--- file_extractor.sh
+++ file_extractor.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 # ====== CONFIG ======
-OUTPUT_DIR="/mnt/c/Users/Prem Karmakar/Desktop/DL_backend/output"
+# Change this path if you want the output folder somewhere else
+OUTPUT_DIR="/mnt/c/Users/Sohamdip/Desktop/Data/Projects/output"
 TIMESTAMP="$(date +'%Y-%m-%d_%H-%M-%S')"
 OUTPUT_FILE="$OUTPUT_DIR/code_base_${TIMESTAMP}.txt"
 
