93ed33bb0378e24248cd945572adac5e63996330
Sohamdip-Santra1 <sohamdipsantra2023@gmail.com>
2026-09-15 05:43:02 +0530

    database wsitched to postgresql then back to sqlite

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:Sarbanidutta%40123@localhost:5432/cephalic_db'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///cephalic.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:Sarbanidutta%40123@localhost:5432/cephalic_db'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///cephalic.db'
 
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:Sarbanidutta%40123@localhost:5432/cephalic_db'
     #SQLALCHEMY_DATABASE_URI= 'sqlite:///cephalic.db'
